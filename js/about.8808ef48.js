a305b3ae2a166a33b05bb1ee11e6a10b566cab10
ahmed hamdy <bme.ahmedhamdy@gmail.com>
2024-01-22 13:59:32 +0200

    add dist

---
+++ js/about.8808ef48.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmy_dream_place"]=self["webpackChunkmy_dream_place"]||[]).push([[443],{6473:function(a,e,n){n.r(e),n.d(e,{default:function(){return d}});var r=n(3396);const u={class:"about"},t=(0,r._)("h1",null,"This is an about page",-1),c=[t];function s(a,e){return(0,r.wg)(),(0,r.iD)("div",u,c)}var i=n(89);const l={},o=(0,i.Z)(l,[["render",s]]);var d=o}}]);
+//# sourceMappingURL=about.8808ef48.js.map
